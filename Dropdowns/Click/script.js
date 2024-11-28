57e7cee06342d88c42dc9504e2fd95d7c8e6e68e
Vikas Kumar <vikaskumar7801n@gmail.com>
2024-11-28 15:10:05 +0530

    first commit

---
+++ Dropdowns/Click/script.js
@@ -0,0 +1,89 @@
+/* Dropdown Click 1 Js */
+/* Dropdown Click 1 Js */
+/* Dropdown Click 1 Js */
+/* Dropdown Click 1 Js */
+
+function Dropdown1() {
+  document.getElementById("myDropdown1").classList.toggle("show1");
+}
+
+/* Dropdown Click 2 Js */
+/* Dropdown Click 2 Js */
+/* Dropdown Click 2 Js */
+/* Dropdown Click 2 Js */
+
+function Dropdown2() {
+  document.getElementById("myDropdown2").classList.toggle("show2");
+}
+
+/* Dropdown Click 3 Js */
+/* Dropdown Click 3 Js */
+/* Dropdown Click 3 Js */
+/* Dropdown Click 3 Js */
+
+function Dropdown3() {
+  document.getElementById("myDropdown3").classList.toggle("show3");
+}
+
+/* Dropdown Click 4 Js */
+/* Dropdown Click 4 Js */
+/* Dropdown Click 4 Js */
+/* Dropdown Click 4 Js */
+
+function Dropdown4() {
+  document.getElementById("myDropdown4").classList.toggle("show4");
+}
+
+/* Dropdown Click 5 Js */
+/* Dropdown Click 5 Js */
+/* Dropdown Click 5 Js */
+/* Dropdown Click 5 Js */
+
+function Dropdown5() {
+  document.getElementById("myDropdown5").classList.toggle("show5");
+}
+
+/* Dropdown Click 6 Js */
+/* Dropdown Click 6 Js */
+/* Dropdown Click 6 Js */
+/* Dropdown Click 6 Js */
+
+function Dropdown6() {
+  document.getElementById("myDropdown6").classList.toggle("show6");
+}
+
+/* Dropdown Click 7 Js */
+/* Dropdown Click 7 Js */
+/* Dropdown Click 7 Js */
+/* Dropdown Click 7 Js */
+
+function Dropdown7() {
+  document.getElementById("myDropdown7").classList.toggle("show7");
+}
+
+/* Dropdown Click 8 Js */
+/* Dropdown Click 8 Js */
+/* Dropdown Click 8 Js */
+/* Dropdown Click 8 Js */
+
+function Dropdown8() {
+  document.getElementById("myDropdown8").classList.toggle("show8");
+}
+
+/* Dropdown Click 9 Js */
+/* Dropdown Click 9 Js */
+/* Dropdown Click 9 Js */
+/* Dropdown Click 9 Js */
+
+function Dropdown9() {
+  document.getElementById("myDropdown9").classList.toggle("show9");
+}
+
+/* Dropdown Click 10 Js */
+/* Dropdown Click 10 Js */
+/* Dropdown Click 10 Js */
+/* Dropdown Click 10 Js */
+
+function Dropdown10() {
+  document.getElementById("myDropdown10").classList.toggle("show10");
+}
---
+++ Dropdowns/Hover/script.js
@@ -0,0 +1,129 @@
+/* Dropdown Hover 1 Js */
+/* Dropdown Hover 1 Js */
+/* Dropdown Hover 1 Js */
+/* Dropdown Hover 1 Js */
+
+function DropdownHover1MouseOver() {
+  document.getElementById("myDropdownHover1").classList.add("showHover1");
+}
+
+function DropdownHover1MouseLeave() {
+  document.getElementById("myDropdownHover1").classList.remove("showHover1");
+}
+
+/* Dropdown Hover 2 Js */
+/* Dropdown Hover 2 Js */
+/* Dropdown Hover 2 Js */
+/* Dropdown Hover 2 Js */
+
+function DropdownHover2MouseOver() {
+  document.getElementById("myDropdownHover2").classList.add("showHover2");
+}
+
+function DropdownHover2MouseLeave() {
+  document.getElementById("myDropdownHover2").classList.remove("showHover2");
+}
+
+/* Dropdown Hover 3 Js */
+/* Dropdown Hover 3 Js */
+/* Dropdown Hover 3 Js */
+/* Dropdown Hover 3 Js */
+
+function DropdownHover3MouseOver() {
+  document.getElementById("myDropdownHover3").classList.add("showHover3");
+}
+
+function DropdownHover3MouseLeave() {
+  document.getElementById("myDropdownHover3").classList.remove("showHover3");
+}
+
+/* Dropdown Hover 4 Js */
+/* Dropdown Hover 4 Js */
+/* Dropdown Hover 4 Js */
+/* Dropdown Hover 4 Js */
+
+function DropdownHover4MouseOver() {
+  document.getElementById("myDropdownHover4").classList.add("showHover4");
+}
+
+function DropdownHover4MouseLeave() {
+  document.getElementById("myDropdownHover4").classList.remove("showHover4");
+}
+
+/* Dropdown Hover 5 Js */
+/* Dropdown Hover 5 Js */
+/* Dropdown Hover 5 Js */
+/* Dropdown Hover 5 Js */
+
+function DropdownHover5MouseOver() {
+  document.getElementById("myDropdownHover5").classList.add("showHover5");
+}
+
+function DropdownHover5MouseLeave() {
+  document.getElementById("myDropdownHover5").classList.remove("showHover5");
+}
+
+/* Dropdown Hover 6 Js */
+/* Dropdown Hover 6 Js */
+/* Dropdown Hover 6 Js */
+/* Dropdown Hover 6 Js */
+
+function DropdownHover6MouseOver() {
+  document.getElementById("myDropdownHover6").classList.add("showHover6");
+}
+
+function DropdownHover6MouseLeave() {
+  document.getElementById("myDropdownHover6").classList.remove("showHover6");
+}
+
+/* Dropdown Hover 7 Js */
+/* Dropdown Hover 7 Js */
+/* Dropdown Hover 7 Js */
+/* Dropdown Hover 7 Js */
+
+function DropdownHover7MouseOver() {
+  document.getElementById("myDropdownHover7").classList.add("showHover7");
+}
+
+function DropdownHover7MouseLeave() {
+  document.getElementById("myDropdownHover7").classList.remove("showHover7");
+}
+
+/* Dropdown Hover 8 Js */
+/* Dropdown Hover 8 Js */
+/* Dropdown Hover 8 Js */
+/* Dropdown Hover 8 Js */
+
+function DropdownHover8MouseOver() {
+  document.getElementById("myDropdownHover8").classList.add("showHover8");
+}
+
+function DropdownHover8MouseLeave() {
+  document.getElementById("myDropdownHover8").classList.remove("showHover8");
+}
+
+/* Dropdown Hover 9 Js */
+/* Dropdown Hover 9 Js */
+/* Dropdown Hover 9 Js */
+/* Dropdown Hover 9 Js */
+
+function DropdownHover9MouseOver() {
+  document.getElementById("myDropdownHover9").classList.add("showHover9");
+}
+
+function DropdownHover9MouseLeave() {
+  document.getElementById("myDropdownHover9").classList.remove("showHover9");
+}
+
+/* Dropdown Hover 10 Js */
+/* Dropdown Hover 10 Js */
+/* Dropdown Hover 10 Js */
+/* Dropdown Hover 10 Js */
+
+function DropdownHover10MouseOver() {
+  document.getElementById("myDropdownHover10").classList.add("showHover10");
+}
+
+function DropdownHover10MouseLeave() {
+  document.getElementById("myDropdownHover10").classList.remove("showHover10");
+}
